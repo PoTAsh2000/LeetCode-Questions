e4f88b2390d2a94b5bf977a2027896542ee92bff
PoTAsh2000 <tharensman@gmail.com>
2025-01-05 13:19:17 +0100

    longest sub string

--- problems/longest_substring_without_repeating_characters/solution.py
+++ problems/longest_substring_without_repeating_characters/solution.py
@@ -4,44 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
-        longest = 0
 
-        chars_checked = {}
-        for index in range(len(s)):
-            char = s[index]
-            
-            
-        return longest
+        longest_substring = 0
+        str_length = len(s)
 
+        for sub_str_start_i in range(str_length):
+            chars_left = str_length - sub_str_start_i
+            if chars_left <= longest_substring:
+                print("substring '" + s[sub_str_start_i:str_length] + "' is less or equel then current longest[" + str(longest_substring) + "], no need to continue")
+                return longest_substring
 
+            processed = []
+            for char_index in range(sub_str_start_i, str_length):
+                char = s[char_index]
 
+                if processed.__contains__(char):
+                    if len(processed) > longest_substring:
+                        longest_substring = len(processed)
+                    break
+                processed.append(char)
 
-
-
-
-
-        # longest_substring = 0
-        # str_length = len(s)
-
-        # for sub_str_start_i in range(str_length):
-        #     chars_left = str_length - sub_str_start_i
-        #     if chars_left <= longest_substring:
-        #         print("substring '" + s[sub_str_start_i:str_length] + "' is less or equel then current longest[" + str(longest_substring) + "], no need to continue")
-        #         return longest_substring
-
-        #     processed = []
-        #     for char_index in range(sub_str_start_i, str_length):
-        #         char = s[char_index]
-
-        #         if processed.__contains__(char):
-        #             if len(processed) > longest_substring:
-        #                 longest_substring = len(processed)
-        #             break
-        #         processed.append(char)
-
-        #         if char_index == str_length - 1:
-        #             print("end of string reached and no duplicates found - valid substring: " + s[sub_str_start_i:str_length])
-        #             return len(processed)
-        # return longest_substring
+                if char_index == str_length - 1:
+                    print("end of string reached and no duplicates found - valid substring: " + s[sub_str_start_i:str_length])
+                    return len(processed)
+        return longest_substring
 
