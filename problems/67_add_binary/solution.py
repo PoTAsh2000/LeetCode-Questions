33cd45052e5f8a343762eaa95f5e7a9c9d98a911
PoTAsh2000 <tharensman@gmail.com>
2025-04-09 00:32:25 +0200

    O(n) solution for problem 67 add binary. Can probably be faster, might need to ga back later

--- problems/67_add_binary/solution.py
+++ problems/67_add_binary/solution.py
@@ -5,24 +5,26 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        iter = 0
-        longestBinaryNumber = max(len(a), len(b))
-        
-        aInt = 0
-        bInt = 0
-        while iter < longestBinaryNumber:
-            if iter < len(a):
-                aInt += int(a[iter]) * (2 ** (len(a) - 1 - iter))
-            if iter < len(b):
-                bInt += int(b[iter]) * (2 ** (len(b) - 1 - iter))
-            iter += 1
-        
-        remainder = aInt + bInt
-        if remainder == 0: return "0"
+        sum = int(self.binaryToInt(a)) + int(self.binaryToInt(b))
 
+        if sum == 0:
+            return "0"
+        
+        return self.intToBinary(sum)
+            
+    def intToBinary(self, remainder):
         result = ""
         while remainder != 0:
             bit = str(remainder % 2)
             result = bit + result
             remainder = remainder // 2
         return result
+
+    def binaryToInt(self, binaryNumber):
+        result = 0
+        power = len(str(binaryNumber)) - 1
+        for i in binaryNumber:
+            result += int(i) * (2 ** power)
+            power -= 1
+        return result
+        
